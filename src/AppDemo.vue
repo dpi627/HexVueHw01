9d4dbd02659f92f7d4c3d4508d24c545181571d0
brian <dpi.studio@gmail.com>
2025-08-02 10:08:00 +0800

    feat: 新增 AppDemo.vue 組件
    - 實作了一個簡單的輸入框，顯示用戶輸入的值
    - 支援單向綁定和 HTML 顯示
    - 增加了顯示輸入值的按鈕功能
    - 更新 main.js 以使用新的 AppDemo.vue 組件

---
+++ src/AppDemo.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const inputValue = ref(1)
+const rawHTML = ref('<span style="color: red;">這是一段 HTML</span>')
+function showValue() {
+  alert(`你輸入的是：${inputValue.value}`)
+}
+</script>
+
+<template>
+  <input v-bind:value="inputValue" type="text" />
+  <p>目前輸入：{{ inputValue }}</p>
+  <p v-text="inputValue"></p>
+  <p v-once>目前輸入：{{ inputValue }} (單向綁定)</p>
+  <p v-html="rawHTML"></p>
+  
+  <button v-on:click="showValue">
+    顯示輸入值
+  </button>
+</template>
+
+<style></style>
