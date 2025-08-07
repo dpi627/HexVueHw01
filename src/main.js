1693088bf6167d595ad3a4259d776c7401e5b297
brian <dpi.studio@gmail.com>
2025-08-07 12:56:49 +0800

    feat: 新增 Daily5.vue 組件並更新 main.js 引入
    - 新增 Daily5.vue 組件，顯示水果資訊
    - 更新 main.js 引入 Daily5.vue 組件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createApp } from 'vue'
 // import './style.css'
-import App from './Daily4.vue'
+import App from './Daily5.vue'
 
 createApp(App).mount('#app')
